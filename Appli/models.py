2764c7e643b68e9d7641c78359288816f3574919
Swaroop <Swaroop.rajlama2001@outlook.com>
2023-08-07 21:41:48 +0530

    Updated backend to work with db

--- Appli/models.py
+++ Appli/models.py
@@ -13,3 +13,54 @@ class Sheet1(models.Model):
     class Meta:
         managed = False
         db_table = 'sheet1'
+
+
+class Sheet2(models.Model):
+    serialno = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=150, blank=True, null=True)
+    origin = models.CharField(max_length=25, blank=True, null=True)
+    meaning = models.CharField(max_length=2000, blank=True, null=True)
+    sentence = models.CharField(max_length=2000, blank=True, null=True)
+    used = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sheet2'
+
+
+class Sheet3(models.Model):
+    serialno = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=150, blank=True, null=True)
+    origin = models.CharField(max_length=25, blank=True, null=True)
+    meaning = models.CharField(max_length=2000, blank=True, null=True)
+    sentence = models.CharField(max_length=2000, blank=True, null=True)
+    used = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sheet3'
+
+class Sheet4(models.Model):
+    serialno = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=150, blank=True, null=True)
+    origin = models.CharField(max_length=25, blank=True, null=True)
+    meaning = models.CharField(max_length=2000, blank=True, null=True)
+    sentence = models.CharField(max_length=2000, blank=True, null=True)
+    used = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sheet4'
+
+
+class Sheet5(models.Model):
+    serialno = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=150, blank=True, null=True)
+    origin = models.CharField(max_length=25, blank=True, null=True)
+    meaning = models.CharField(max_length=2000, blank=True, null=True)
+    sentence = models.CharField(max_length=2000, blank=True, null=True)
+    used = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sheet5'
--- Appli/views.py
+++ Appli/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
-from .models import Sheet1
-from random import randrange
+from . import models
+import random
 
-def word_list(request):
-    words = Sheet1.objects.all()
-    n = randrange(len(words))
-    generated_word = words[n]
+def word_generator_sheet1(request):
+    generated_word = models.Sheet1.objects.all()
+    return random.choice(generated_word)
+
+
+def word_generator_sheet2(request):
+    generated_word = models.Sheet2.objects.all()
+    return random.choice(generated_word)
+
+
+def word_generator_sheet3(request):
+    generated_word = models.Sheet3.objects.all()
+    return random.choice(generated_word)
+
+
+def word_generator_sheet4(request):
+    generated_word = models.Sheet4.objects.all()
+    return random.choice(generated_word)
+
+
+def word_generator_sheet5(request):
+    generated_word = models.Sheet5.objects.all()
+    return random.choice(generated_word)
+
+
+def display(request):
+    generated_word = None
+    if request.method == "POST":
+        sheet = request.POST.get("Sheet")
+        if sheet == 'Sheet1':
+            generated_word = word_generator_sheet1(request)
+        elif sheet == 'Sheet2':
+            generated_word = word_generator_sheet2(request)
+        elif sheet == 'Sheet3':
+            generated_word = word_generator_sheet3(request)
+        elif sheet == 'Sheet4':
+            generated_word = word_generator_sheet4(request)
+        elif sheet == 'Sheet5':
+            generated_word = word_generator_sheet5(request)
+        else:
+            return render(request, 'Appli/word_list.html', {'generated_word':None})
+    
     return render(request, 'Appli/word_list.html', {'generated_word':generated_word})
