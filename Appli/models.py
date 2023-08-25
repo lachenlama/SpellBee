efebedc18468dd37aa94d4a3b05f367bb4d39d0e
Swaroop <Swaroop.rajlama2001@outlook.com>
2023-08-25 10:21:42 +0530

    Modifications on models

--- Appli/models.py
+++ Appli/models.py
@@ -28,17 +28,17 @@ class Sheet2(models.Model):
         db_table = 'sheet2'
 
 
-# class Sheet3(models.Model):
-#     serialno = models.AutoField(primary_key=True)
-#     word = models.CharField(max_length=150, blank=True, null=True)
-#     origin = models.CharField(max_length=25, blank=True, null=True)
-#     meaning = models.CharField(max_length=2000, blank=True, null=True)
-#     sentence = models.CharField(max_length=2000, blank=True, null=True)
-#     used = models.BooleanField(blank=True, null=True)
+class Sheet3(models.Model):
+    serialno = models.AutoField(primary_key=True)
+    word = models.CharField(max_length=200, blank=True, null=True)
+    origin = models.CharField(max_length=25, blank=True, null=True)
+    meaning = models.CharField(max_length=3000, blank=True, null=True)
+    sentence = models.CharField(max_length=3000, blank=True, null=True)
+    used = models.BooleanField(blank=True, null=True)
 
-#     class Meta:
-#         managed = False
-#         db_table = 'sheet3'
+    class Meta:
+        managed = False
+        db_table = 'sheet3'
 
 class Sheet4(models.Model):
     serialno = models.AutoField(primary_key=True)
